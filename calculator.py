78b516ca5b9ff260ba50323c0d05aaa08cfc9037
WildLochu <109187092+BigMoistLochu@users.noreply.github.com>
2024-06-17 02:02:28 +0200

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     if len(sys.argv) != 3:
         print("Usage: python multiply.py <num1> <num2>")
         sys.exit(1)
-# wywolaj update piplinu
+# wywolaj update piplinu1
     num1 = float(sys.argv[1])
     num2 = float(sys.argv[2])
 
